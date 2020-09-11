79f0c240217151931dadda66425c813cd846543d
Ronald Otieno <ronnlats@gmail.com>
2020-09-11 14:26:29 +0300

    rooms and section view added

--- app.js
+++ app.js
@@ -150,12 +150,31 @@ window.addEventListener("DOMContentLoaded", function(e){
 
 // });
 
+//image gallery
+
+
+function imageGallery(){
+    const highlight = document.querySelector('.gallery-highlight');
+    const previews = document.querySelectorAll('.room-preview img');
+
+    previews.forEach(preview =>{
+        preview.addEventListener('click', function(){
+            const smallSrc = this.src;
+            //the power of replace
+            const bigSrc = smallSrc.replace("small", "big");
+            highlight.src = bigSrc;
+            previews.forEach(preview => preview.classList.remove("room-active"));
+            preview.classList.add("room-active")
+        })
+    })
+};
 
 //The functions executioner
 const app = () => {
     carousel();
-    slideShow();
+    imageGallery();
     navSlide();
+
     // showChefs();
     //scr();
 };
